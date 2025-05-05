74f834e771c04b93fca91574f75c1c9a4974b4fa
aztec-dev <aztech.dev1@gmail.com>
2025-05-05 14:55:19 +1000

    wip: added landing card information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 // import Image from "next/image";
-
+import Landing from "@/ui/landing";
+import Card from "@/ui/card";
 export default function Home() {
   return (
-      <h1>Glorious Medical Center - Home</h1>
+      <section>
+        {/*<h1>Glorious Medical Center - Home</h1>*/}
+
+          <Card className={"mt-2"}>
+              <Landing />
+          </Card>
+      </section>
   );
 }
