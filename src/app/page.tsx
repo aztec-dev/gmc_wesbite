e07c9b697af5584f3f1a7a3a67a2ba34c124c743
aztec-dev <aztech.dev1@gmail.com>
2025-05-06 17:26:50 +1000

    fix: updated html semantics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
-// import Image from "next/image";
 import Landing from "@/ui/landing";
 import Card from "@/ui/card";
-export default function Home() {
-  return (
-      <section>
-        {/*<h1>Glorious Medical Center - Home</h1>*/}
 
-          <Card className={"mt-2"}>
-              <Landing />
-          </Card>
-      </section>
-  );
+export default function Home() {
+    return (
+        <main className="space-y-8">
+            <section className="h-screen">
+                <Card className="mt-2">
+                    <Landing />
+                </Card>
+            </section>
+        </main>
+    );
 }
