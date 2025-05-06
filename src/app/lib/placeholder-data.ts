c6e6a902f1aa71fc48ecc5eb9e05668390618f0f
aztec-dev <aztech.dev1@gmail.com>
2025-05-06 17:25:13 +1000

    init

---
+++ src/app/lib/placeholder-data.ts
@@ -0,0 +1,24 @@
+// This file contains placeholder data that represents the employees (doctors, nurses, etc.)
+// of Glorious Medical Center.
+const employees = [
+    {
+        id: 'a8079097-1643-4e24-be41-9650f35e6d8e',
+        first_name: 'John',
+        last_name: 'Doe',
+        image_url: '/employees/john_doe.jpg',
+    },
+    {
+        id: '9c411b93-8a9a-4386-81e6-90d139800ad0',
+        first_name: 'Lorem',
+        last_name: 'Ipsum',
+        image_url: '/employees/lorem_ipsum.jpg',
+    },
+    {
+        id: 'dc1be3f6-e313-4a3b-8c37-37c153206d0f',
+        first_name: 'James',
+        last_name: 'Bond',
+        image_url: '/employees/james_bond.jpg',
+    }
+]
+
+export {employees};
